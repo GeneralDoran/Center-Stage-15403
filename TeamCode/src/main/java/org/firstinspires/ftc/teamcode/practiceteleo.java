153f50c246ddc43de08931555ea74d87a600dd42
Riley-jpg <carlson.riley24@libertycommon.org>
2023-12-20 13:04:43 -0700

    auto?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practiceteleo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practiceteleo.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.Programs.Auto_Util;
+import org.firstinspires.ftc.teamcode.Auto_Util;
 
 
 @TeleOp(name="teenage Teleop", group="Pushbot")
